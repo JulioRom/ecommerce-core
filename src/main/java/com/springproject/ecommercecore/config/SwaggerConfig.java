4f65569473c940931a020ef92ba22539dbe4fb23
Julio <julioandrescampos@gmail.com>
2025-02-18 01:18:39 -0300

    configurando swagger

--- src/main/java/com/springproject/ecommercecore/config/SwaggerConfig.java
+++ src/main/java/com/springproject/ecommercecore/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.springproject.ecommercecore.config;
 
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,6 +18,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Ecommerce API")
                         .version("1.0")
-                        .description("Documentación de la API | Proyecto de e-commerce"));
+                        .description("Documentación de la API | Proyecto de e-commerce"))
+                .servers(List.of(new Server().url("https://ecommerce-core-production.up.railway.app").description("Servidor de Producción")));
     }
 }
