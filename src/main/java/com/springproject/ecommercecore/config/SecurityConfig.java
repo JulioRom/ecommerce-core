7c2643ca90eb70db3cb5edf1fae429fa6c25fa23
Julio <julioandrescampos@gmail.com>
2025-02-17 23:36:51 -0300

    actualizando prod properties

--- src/main/java/com/springproject/ecommercecore/config/SecurityConfig.java
+++ src/main/java/com/springproject/ecommercecore/config/SecurityConfig.java
@@ -34,14 +34,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // 🔹 Habilitar CORS
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Habilitar CORS
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
                         .requestMatchers("/api/auth/login", "/api/auth/register").permitAll() // Permitir login y registro
-                        .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN") // 🔹 Cambiado de hasRole a hasAuthority
-                        .requestMatchers("/api/usuarios/**").hasAuthority("ROLE_ADMIN") // 🔹 Cambiado de hasRole a hasAuthority
-                        .requestMatchers("/api/productos/**").hasAuthority("ROLE_ADMIN") /// 🔹 Cambiado de hasRole a hasAuthority
+                        .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN") // Cambiado de hasRole a hasAuthority
+                        .requestMatchers("/api/usuarios/**").hasAuthority("ROLE_ADMIN") // Cambiado de hasRole a hasAuthority
+                        .requestMatchers("/api/productos/**").hasAuthority("ROLE_ADMIN") /// Cambiado de hasRole a hasAuthority
                         .requestMatchers("/api/carrito/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
